2cc3b78bd967571239f9be7422e453d924f1560a
Imaad Feerasta <feerasta@usc.edu>
2022-11-08 07:39:47 +0000

    Checkpoint

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -4,13 +4,31 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 int calculateHeight(Node* root);
-
+int max(int x, int y); //not sure if allowed #include <algorithm>
 bool equalPaths(Node * root)
 {
     // Add your code below
-    if (root == nullptr) return false;
-    else if (root->left == nullptr && root->right == nullptr) return true;
-    else return (equalPaths(root->left) && equalPaths(root->right));
+    if (root == NULL) return true;
+
+    if (root->left == NULL && root->right == NULL) return true;
+
+    if (root->left == NULL) return calculateHeight(root->right) == 1;
+
+    if (root->right == NULL) return calculateHeight(root->left) == 1;
+
+    return ((calculateHeight(root->right) == calculateHeight(root->left)) && equalPaths(root->left) && equalPaths(root->right));
+    
+}
+
+int calculateHeight(Node* root) {
+    if (root == NULL) return 0;
+    return (1 + max(calculateHeight(root->left), calculateHeight(root->right)));
+}
+
+int max(int x, int y) {
+    if (x > y) return x;
+    else if (y > x) return y;
+    else return x;
 }
 
 
