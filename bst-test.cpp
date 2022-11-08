9d13a68ecc1f39b74624d6da65e895fc48987d28
Imaad Feerasta <feerasta@usc.edu>
2022-11-08 07:52:49 +0000

    Checkpoint

--- bst-test.cpp
+++ bst-test.cpp
@@ -12,25 +12,32 @@ int main(int argc, char *argv[])
     BinarySearchTree<char,int> bt;
     bt.insert(std::make_pair('a',1));
     bt.insert(std::make_pair('b',2));
-    
+    bt.insert(std::make_pair('c',3));
+		bt.insert(std::make_pair('c',7));
     cout << "Binary Search Tree contents:" << endl;
     for(BinarySearchTree<char,int>::iterator it = bt.begin(); it != bt.end(); ++it) {
         cout << it->first << " " << it->second << endl;
     }
+
     if(bt.find('b') != bt.end()) {
         cout << "Found b" << endl;
     }
     else {
         cout << "Did not find b" << endl;
     }
+    
+
     cout << "Erasing b" << endl;
     bt.remove('b');
 
-    // AVL Tree Tests
+  
+    cout << "AVL TREE TESTS" << endl;
+
+		bt.print();
     AVLTree<char,int> at;
     at.insert(std::make_pair('a',1));
     at.insert(std::make_pair('b',2));
-
+    
     cout << "\nAVLTree contents:" << endl;
     for(AVLTree<char,int>::iterator it = at.begin(); it != at.end(); ++it) {
         cout << it->first << " " << it->second << endl;
